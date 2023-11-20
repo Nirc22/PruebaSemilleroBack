943ecb64731af53751ff55e71ce9ca491ee8282d
Nicolas Rodriguez <nircandsim@gmail.com>
2023-11-20 01:23:49 -0500

    TOkenUtils ok

---
+++ src/main/java/com/semillero/pruebaSemillero/security/TokenUtils.java
@@ -0,0 +1,48 @@
+package com.semillero.pruebaSemillero.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+
+import java.util.*;
+import java.util.jar.JarException;
+
+public class TokenUtils {
+
+    //se puede colocar en aplication-properties
+    private final static String ACCESS_TOKEN_SECRET = "asdfasdf231231lkj123";
+    private final static Long ACCESSS_TOKEN_VALIDITY_SECONDS = 28_800L;
+
+    public static String crearToken(String nombre, String email){
+        long expirationTime = ACCESSS_TOKEN_VALIDITY_SECONDS * 1000;
+        Date expirationDate = new Date(System.currentTimeMillis() + expirationTime);
+
+        Map<String, Object> extra = new HashMap<>();
+        extra.put("nombre", nombre);
+
+        return Jwts.builder()
+                .setSubject(email)
+                .setExpiration(expirationDate)
+                .addClaims(extra)
+                .signWith(Keys.hmacShaKeyFor(ACCESS_TOKEN_SECRET.getBytes()))
+                .compact();
+    }
+
+    public static UsernamePasswordAuthenticationToken getAuthentication(String token){
+        try {
+            Claims claims = Jwts.parserBuilder()
+                    .setSigningKey(ACCESS_TOKEN_SECRET.getBytes())
+                    .build()
+                    .parseClaimsJws(token)
+                    .getBody();
+
+            String email = claims.getSubject();
+
+            return new UsernamePasswordAuthenticationToken(email, null, Collections.emptyList());
+        }catch (JwtException e){
+            return null;
+        }
+    }
+}
