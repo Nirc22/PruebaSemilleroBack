0f64aefafb0cbe67161413271e10914efcacd348
Nicolas Rodriguez <nircandsim@gmail.com>
2023-12-14 23:09:14 -0500

    Backend OK - ObetenerpacienteId OK

---
+++ src/main/java/com/semillero/pruebaSemillero/models/RafPersonasModel.java
@@ -0,0 +1,10 @@
+package com.semillero.pruebaSemillero.models;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "raf_personas")
+public class RafPersonasModel {
+}
